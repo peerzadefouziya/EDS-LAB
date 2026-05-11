f1734cabaee03ffa85e37e66c01a33493b10872e
peerzadefouziya <peerzadefouziya5@gmail.com>
2026-05-11 20:02:59 +0530

    main

---
+++ Calculate Momentum.py
@@ -0,0 +1,11 @@
+# Calculate Momentum
+
+# Input
+mass = float(input())
+velocity = float(input())
+
+# Calculation
+momentum = mass * velocity
+
+# Output (rounded to 2 decimal places)
+print(f"{momentum:.2f}kgm/s")
